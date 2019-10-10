5bc7fd7a555a4ae43b0f26cac1a40b616c82c02b
petivip <petivip@gmail.com>
2019-10-09 20:01:01 -0400

    springboot301

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    @NotNull
+    private String name;
+
+    @Min(1)
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
